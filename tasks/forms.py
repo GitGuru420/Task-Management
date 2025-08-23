6468f368ee75749a4a7046c90d1c605f9cd03745
GitGuru420 <raisulislam1892@gmail.com>
2025-08-23 09:56:03 +0600

    Submit Task into database using form data

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,11 +2,11 @@ from django import forms
 
 class TaskForm(forms.Form):
     title = forms.CharField(max_length=100, label="Task Title")
-    description = forms.CharField(widget=forms.Textarea, label="Task Description")
+    descriptions = forms.CharField(widget=forms.Textarea, label="Task Description")
     due_date = forms.DateField(widget=forms.SelectDateWidget, label="Due Date")
-    assigned_to = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=[], label="Assigned To")
+    employoee = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=[], label="Assigned To")
     
     def __init__(self, *args, **kwargs):
         employees = kwargs.pop("employees", [])
         super().__init__(*args, **kwargs)
-        self.fields['assigned_to'].choices = [(emp.id, emp.name) for emp in employees]
+        self.fields['employoee'].choices = [(emp.id, emp.name) for emp in employees]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm
-from tasks.models import Employee
+from tasks.models import Employee, Task
 
 # App Folder Templates
 def manager_dashboard(request):
@@ -22,5 +22,25 @@ def static_test(request):
 def submit_task(request):
     employees = Employee.objects.all()
     form = TaskForm(employees=employees)
+    
+    # For POST
+    if request.method == "POST":
+        form = TaskForm(request.POST, employees=employees)
+        if form.is_valid():
+            data = form.cleaned_data
+            title = data.get('title')
+            descriptions = data.get('descriptions')
+            due_date = data.get('due_date')
+            employoee = data.get('employoee')
+            print(employoee)
+            
+            task = Task.objects.create(title=title, descriptions=descriptions, due_date=due_date)
+            
+            # assigne employee to tasks
+            for emp_id in employoee:
+                employee = Employee.objects.get(id=emp_id)
+                task.employoee.add(employee)
+                
+            return HttpResponse("Task Added Successfully")
     context = {"form": form}
     return render(request, "submit_form.html", context)
